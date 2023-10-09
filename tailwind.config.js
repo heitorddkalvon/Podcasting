e9fd70b837123338ad75446576c0b368ecd98703
Heitor <heitorkalvon@gmail.com>
2023-10-09 20:12:33 -0300

    2.0
    AAAAAAAAAAAAA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      colors: {
-        darkslateblue: {
-          "100": "#33317a",
-          "200": "#1d1b49",
-        },
-        white: "#fff",
-      },
-      spacing: {},
-      fontFamily: {
-        "istok-web": "'Istok Web'",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-    fontSize: {
-      inherit: "inherit",
-    },
   },
-  corePlugins: {
-    preflight: false,
-  },
-};
+  plugins: [],
+}
